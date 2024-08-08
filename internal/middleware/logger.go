bb2c2a68a1e1ec190a58a81fb2447d5f53e89d4a
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-08-08 07:34:58 +0530

    feat:Added a custom logger

---
+++ internal/middleware/logger.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func (m *Middleware) StyledLogger() fiber.Handler {
+	logFile, err := os.OpenFile("requests.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	logConfig := logger.Config{
+		Done: func(c *fiber.Ctx, logString []byte) {
+			fmt.Print(string(logString))
+		},
+		Output: logFile,
+	}
+
+	return logger.New(logConfig)
+}
